1d7a5994a62a1225d255d3fca84c30a1a849f718
Andrew Brown <brownand000@gmail.com>
2023-12-19 11:57:31 -0600

    added event listeners to numbered buttons. stored value and displayed nums

--- main.js
+++ main.js
@@ -9,7 +9,21 @@ const divide = (a,b) => a/b;
 let num1;
 let num2;
 let oper;
+let displayVal = '';
 
 function operate(oper,num1,num2) {
     return oper(num1,num2);
 }
+
+const btnNum = document.querySelectorAll('.btnNum');
+const display = document.querySelector('#display');
+
+// Stores value for each number button in displayVal;
+
+btnNum.forEach(button => {
+    const val = button.textContent;
+    button.addEventListener('click', () =>{
+        displayVal += val;
+        display.textContent = displayVal;
+    } )
+})
